a513dfca7db351f579f68bfad926a9b7a0893005
megado123 <megado123@gmail.com>
2018-03-31 06:05:31 -0500

    Update run.py

--- SampleFunction/run.py
+++ SampleFunction/run.py
@@ -18,11 +18,12 @@ with open(os.environ["REQ"]) as req:
 # see https://github.com/Azure/azure-webjobs-sdk-script/issues/586
 repository = docdb.Repository(os.environ["DOCDB_HOST"], os.environ["DOCDB_KEY"])
 
+print(details)
 # using email as partition key, so ensure that it exists,
 # even if it's made up :)
 
-#if not details.has_key("email"):
-#    details["email"] = "%s@outlook.com" % uuid.uuid4()
+if not details.has_key("email"):
+    details["email"] = "%s@outlook.com" % uuid.uuid4()
 
 print ("Storing the contact details in Azure Document DB")
 doc = repository.insert(details)
