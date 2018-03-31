7bc78db931f0bed5a9350ce3f0ba38d08203a013
megado123 <megado123@gmail.com>
2018-03-31 05:34:45 -0500

    Update pip-install.cmd

--- pip-install.cmd
+++ pip-install.cmd
@@ -2,4 +2,4 @@ import sys, os.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'env/Lib/site-packages')))
 
 python -m pip install -r requirements.txt
-print("instaslling requirements.txt")
+
